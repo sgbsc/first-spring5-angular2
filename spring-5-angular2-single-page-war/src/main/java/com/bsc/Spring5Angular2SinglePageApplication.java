6d95657e66bb79f6bff45c96586362a641bbbc45
sgbsc <44087056+sgbsc@users.noreply.github.com>
2018-11-16 14:44:19 +0000

    Initial Commit

---
+++ spring-5-angular2-single-page-war/src/main/java/com/bsc/Spring5Angular2SinglePageApplication.java
@@ -0,0 +1,23 @@
+package com.bsc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Angular2SinglePageApplication implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5Angular2SinglePageApplication.class, args);
+	}
+	
+	//bootstap database at start of application
+	public void run(String... strings) throws Exception {
+		userRepository.save(new User("Mario","Romano"));
+		userRepository.save(new User("Ralf","Verdi"));
+	}
+}
